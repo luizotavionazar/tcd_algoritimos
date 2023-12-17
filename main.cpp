f0963455fe44ece372ebf473005008d4947c3b48
luizotavionazar <147573027+luizotavionazar@users.noreply.github.com>
2023-12-16 23:46:05 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <locale.h>
 #include "estruturas.h"
 using namespace std;
+#define QTDEVT 50
 void linha();
 void linhad();
 void spc();
@@ -44,13 +45,14 @@ void menu(){//Menu de opções
     cout<<"Informe a opção desejada: ";}
 int verifica_evento(evento festa[]){
     int i;
-    for(i=0; i<50; i++){
+    for(i=0; i<QTDEVT; i++){
         if(festa[i].id==0)
         return i;}
     if (i==0){
         cout<<"Já foram cadastrados todos os 50 eventos!"<<endl;
-        cout<<"Sinto muito :("<<endl;}}
-void cadastra_evento(evento festa[], int i, int sis, int param1) {
+        cout<<"Sinto muito :("<<endl;
+        return -1;}}
+void cadastra_evento(evento festa[], int i, int sis, int param1){//1° Opção
     spc();
     linhad();
     cout<<"                               CADASTRO DE EVENTOS" << endl;
@@ -71,21 +73,74 @@ void cadastra_evento(evento festa[], int i, int sis, int param1) {
         cin>>festa[i].ingre;
         cout<<"Custo de organização: ";
         cin>>festa[i].custo;
-        cout<<"O evento é open bar? 1-Sim/0-Não: ";
+        cout<<"O evento é Open Bar? 1-Sim/0-Não: ";
         cin>>festa[i].openBar;
-        if (festa[i].openBar){//Calcular os valores de entrada(popu, norm, espec)
-            festa[i].popu=festa[i].custo/festa[i].ingre+1.75*festa[i].drinks[0].valor;}
+        if (festa[i].openBar) {
+            festa[i].popu = festa[i].custo/festa[i].ingre;
+            for (int j=0; j<4; j++) {
+                cout<<"  Cadastro da "<<j+1<<"º bebida:"<<endl;
+                cout<<"    Nome: ";
+                cin>>festa[i].drinks[j].nome;
+                cout<<"    Teor alcoólico: ";
+                cin>>festa[i].drinks[j].teor;
+                cout<< "    Valor: ";
+                cin>>festa[i].drinks[j].valor;
+                festa[i].popu+=1.75*festa[i].drinks[j].valor;}}
         else {
-            festa[i].popu=festa[i].custo/festa[i].ingre;}
-        festa[i].norm=festa[i].popu*1.1;//Calcular valores de entrada normal e especial
+            festa[i].popu = festa[i].custo / festa[i].ingre;}
+        festa[i].norm=festa[i].popu*1.1;//Calcula os valores de entrada normal e especial
         festa[i].espec=festa[i].norm*1.15;
         spc();
         cout<<"                          EVENTO CADASTRADO COM SUCESSO"<<endl;
         linha();
-        cout<<"Iniciando cadastro do próximo evento..."<<endl;
-        spc();
-        cout<<"Identificador único: ";
-        cin>>festa[i].id;}
+        i=verifica_evento(festa);
+        if(i != -1) {
+            cout<<"Iniciando cadastro do próximo evento..."<<endl;
+            spc();
+            cout<<"Identificador único: ";
+            cin>>festa[i].id;}
+        else {
+        cout << "Todos os eventos já foram cadastrados!" << endl;}}
+    spc();
+    cout<<"Fim de execução! Tecle 'Enter' para retornar ao Menu...";
+    getchar();
+    getchar();
+    clstel(sis,param1);}
+evento *identifica_evento(evento festa[],int ident){
+    for(int i=0; i<50; i++){
+        if (festa[i].id==ident){
+            cout<<"Evento encontrado!"<<endl;
+            spc();
+            return &festa[i];}}
+    return nullptr;}
+void pesquisa_evento(evento festa[], int sis, int param1){
+    int ident;
+    spc();
+    linhad();
+    cout<<"                               PESQUISA DE EVENTOS" << endl;
+    linhad();
+    cout<<"Informe o identificador do evento desejado::"<< endl;
+    spc();
+    cout<<"Identificador único: ";
+    cin>>ident;
+    evento *indice=identifica_evento(festa,ident);
+    if (indice!=nullptr){
+        cout<< "Nome: "<<indice->nome<<endl;
+        cout<< "Local: "<<indice->local<<endl;
+        cout<< "Data: "<<indice->data <<endl;
+        cout<< "Quantidade de ingressos: "<<indice->ingre<<endl;
+        if (indice->openBar==1){
+            cout<<"  Evento Open Bar!"<<endl;
+            for (int j=0; j<4; j++) {
+            cout<<"    Bebida "<<j+1<<": "<<indice->drinks[j].nome<<endl;
+            cout<<"    Teor alcoólico: "<<indice->drinks[j].teor<<endl;
+            cout<<"    Valor: "<<indice->drinks[j].valor<<endl;}}
+        else{
+            cout<<"  Evento não é Open Bar!"<<endl;}}
+    else{
+        cout<<"Evento não encontrado na base de dados!"<<endl;}
+    spc();
+    linha();
     spc();
     cout<<"Fim de execução! Tecle 'Enter' para retornar ao Menu...";
     getchar();
@@ -94,7 +149,7 @@ void cadastra_evento(evento festa[], int i, int sis, int param1) {
 int main(){
     setlocale(LC_ALL,"Portuguese");
     int param1=2,opcao,sis,i,opc;
-    evento festa[50];
+    evento festa[QTDEVT];
     double total;
     //=====Identifica o sistema operacional=====
     #if defined(_WIN32)||defined(_WIN64)
@@ -111,12 +166,13 @@ do {
         case 1:
             clstel(sis,param1);
             i=verifica_evento(festa);
-            if(i!=-1)
+            if(i!=-1){
                 cout<<"Iniciando a execução...";
-                cadastra_evento(festa,i,sis,param1);
+                cadastra_evento(festa,i,sis,param1);}
             break;
         case 2:
             clstel(sis,param1);
+            pesquisa_evento(festa,sis,param1);
             cout<<"Iniciando a execução...";
             //atv2(sis,param1);
             break;
